f8e9404fbcbefb26401c5a5075182d2eb49d8a63
Yaroslav Tamliani <yaroslav.tamliane@yandex.ru>
2026-06-28 18:31:59 +0700

    Настроил ewm-main-service как Eureka/Config client и добавил зависимости Config Client + LoadBalancer для обращения к stats-server через service discovery.
    Исправил конфигурацию клиента статистики: теперь используется @LoadBalanced RestTemplate, а URL статистики берётся из централизованной настройки stats.server.url с fallback на http://stats-server.
    
    Настроил stats-server как Eureka/Config client и вынес его runtime-настройки в Config Server; сервис запускается на случайном порту.
    
    Настроил Config Server на случайный порт, регистрацию в Eureka и корректные search-locations, включая конфиги core, stats и infra/gateway.
    
    Исправил централизованную конфигурацию ewm-main-service: сервис запускается на случайном порту и обращается к статистике по имени сервиса stats-server.
    
    Настроил Eureka Discovery Server с именем приложения и корректными параметрами standalone-сервера.
    
    Добавил gateway-server: зависимости Gateway/Eureka/Config, Spring Boot entrypoint, bootstrap через Config Server и маршруты на API ewm-main-service через lb://ewm-main-service; gateway ожидает запросы на порту 8080.

--- core/ewm-main-service/src/main/java/ru/practicum/ewm/common/StatsClientConfig.java
+++ core/ewm-main-service/src/main/java/ru/practicum/ewm/common/StatsClientConfig.java
@@ -1,8 +1,10 @@
 package ru.practicum.ewm.common;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import ru.practicum.client.StatsClient;
 
 @Configuration
@@ -11,7 +13,13 @@ public class StatsClientConfig {
     private String statsServerUrl;
 
     @Bean
-    public StatsClient statsClient() {
-        return StatsClient.create(statsServerUrl);
+    @LoadBalanced
+    public RestTemplate statsRestTemplate() {
+        return new RestTemplate();
+    }
+
+    public StatsClient statsClient(@Value("${stats.server.url:http://stats-server}") String statsServerUrl,
+                                   RestTemplate statsRestTemplate) {
+        return new StatsClient(statsServerUrl, statsRestTemplate);
     }
 }
