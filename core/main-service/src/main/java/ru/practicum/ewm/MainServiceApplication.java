61a0e6ac50f53ff153fd716af40e15d82ecbe271
Yaroslav Tamliani <yaroslav.tamliane@yandex.ru>
2026-06-30 22:15:50 +0700

    Изменил название основного класса Изменил pom файл

--- core/main-service/src/main/java/ru/practicum/ewm/MainServiceApplication.java
+++ core/main-service/src/main/java/ru/practicum/ewm/MainServiceApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class EwmMainServiceApp {
+public class MainServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EwmMainServiceApp.class, args);
+        SpringApplication.run(MainServiceApplication.class, args);
     }
 }
