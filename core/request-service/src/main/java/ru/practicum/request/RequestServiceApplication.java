2276b66a0c1cd2400f1b8cdbe8d28e8750dc11c2
Yaroslav Tamliani <yaroslav.tamliane@yandex.ru>
2026-07-23 21:28:09 +0700

    Внесены изменения в файлы .yaml на сервере и модуле статистики

--- core/request-service/src/main/java/ru/practicum/request/RequestServiceApplication.java
+++ core/request-service/src/main/java/ru/practicum/request/RequestServiceApplication.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+import ru.practicum.client.CollectorClient;
 
 @EnableFeignClients(basePackages = "ru.practicum.request.client")
 @EnableDiscoveryClient
+@Import(CollectorClient.class)
 @SpringBootApplication(scanBasePackages = {
         "ru.practicum.request",
         "ru.practicum.ewm.error",
-        "ru.practicum.ewm.common",
-        "ru.practicum.client"
+        "ru.practicum.ewm.common"
 })
 public class RequestServiceApplication {
     public static void main(String[] args) {
